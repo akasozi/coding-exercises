85d96fe47d1b3c0e5c92c631b4dba4e8d9794f5b
abukasozi <abukasozi@gmail.com>
2025-01-01 14:13:21 +0300

    Initial Commit

---
+++ src/test/java/co/ke/cloudflow/codingexercises/CodingExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ke.cloudflow.codingexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
